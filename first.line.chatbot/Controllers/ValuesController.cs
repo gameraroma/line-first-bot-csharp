583b9a5f805d5a9046829c66f34b3d2e08037725
DESKTOP-R6T0CSH\Pc <gameraroma@gmail.com>
2017-12-25 12:15:55 +0700

    make some change

--- first.line.chatbot/Controllers/ValuesController.cs
+++ first.line.chatbot/Controllers/ValuesController.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Line.Messaging;
+using Line.Messaging.Webhooks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols;
 
 namespace first.line.chatbot.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     public class ValuesController : Controller
     {
         // GET api/values
@@ -27,7 +31,34 @@ namespace first.line.chatbot.Controllers
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            var lineBotApi = new LineMessagingClient("");
+            
         }
+
+       //[HttpPost]
+       //public async Task<HttpResponseMessage> Post(HttpRequestMessage request)
+       //{
+       //    //var events = await request.GetWebhookEventsAsync("");
+       //    //var connectionString = ConfigurationManager<String>.AppSettings["StorageConnectionString"];
+       //    //var blobStorage = await BlobStorage.CreateAsync(connectionString, "linebotcontainer");
+       //    //var eventSourceState = await TableStorage<EventSourceState>.CreateAsync(connectionString, "eventsourcestate");
+       //
+       //    //var app = new LineBotApp(lineMessagingClient, eventSourceState, blobStorage);
+       //    //await app.RunAsync(events);
+       //
+       //    var lineBotApi = new LineMessagingClient("");
+       //
+       //    foreach (WebhookEvent ev in events)
+       //    {
+       //        if (ev is MessageEvent && ev.Mess)
+       //        lineBotApi.ReplyMessageAsync();
+       //    }
+       //
+       //    //lineBotApi.ReplyMessageAsync(events)
+       //
+       //    return Request.CreateResponse(HttpStatusCode.OK);
+       //}
+
 
         // PUT api/values/5
         [HttpPut("{id}")]
